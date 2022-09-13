f2231937cbc4f443277950f837e7cf3d0d3c0437
Ibrahim Coban <ibrahimcoban272941@gmail.com>
2022-09-13 22:08:54 +0200

    neue lösungen

--- app.js
+++ app.js
@@ -291,11 +291,161 @@ substr() yöntemi, belirtilen bir konumda başlar ve belirtilen sayıda karakter
 substr() yöntemi, orijinal dizeyi değiştirmez.
 
 Dizenin sonundan karakterleri çıkarmak için negatif bir başlangıç konumu kullanın. */
-
+/*--------------------------------------------------------- */
+// *******girilen string ifadenin kelimelerinin ilk ve son harfi haric diger harflerini gizleyen bir fonksiyon yazin**********.
 const vorName = 'Ibrahim Coban'.split(' ');
 let newName = '';
 for (i of vorName) {
-  newName += i.split('')[0] + '*'.repeat(i.split('').length - 1) + ' ';
+  newName +=
+    i.split('')[0] +
+    '*'.repeat(i.split('').length - 2) +
+    i.split('')[i.split('').length - 1] +
+    ' ';
+}
+0;
+console.log(newName);
+
+/* 
+-------------   REGEX     -----------
+
+Düzenli olmayan verileri düzenli bir sekilde almamizi saglayan ifadelerdir.Bütün yazilim dillerinde aynidir.
+js de =>
+#validation da kullanilabilir. 
+#regex de yazilan ifade ile bizim girdigimiz verileri karsilastirma ve regex e göre veriyi ayiklama da kullanilir.
+# --g-- global flag i secilmez ise ilk aradigimiz elemanlarin sadece ilkini göstrerir
+
+# -- * -- opsiyonel ve sonraki harfleri secer
+# -- . -- girilen ifadeki karakterleri teker teker secer.
+# -- + -- karekteri tek grup halinde secer.bosluga kadar
+# -- w -- alfanumerik olanlari --W-- ise olmayanlari secer(_ haric)
+#-- s-- sadece space leri --S-- space olmayan herseyi alir
+# --d--  sadece sayilari secer
+# /[a-zA-z0-9]/ ifadesi a dan z ye 0 dan 9 a bütün ifadeleri sec demektir. Burada [] ifadesi secici range tir.
+# --- ^ --- birinci satirin satirbasi ifadesini secer.tüm satirlarin basini secmek icin multiline flagini secmek gerekir(m)
+# gruplama () ile yapilir
+# $ ile sondaki ifade test edilir. /\.$/ sonunda nokta varsa demektir
+# /.(?=r)/ devaminda r olan harfleri sec /.(?!r)/ devaminda r olmayanlari sec(lookahead)
+# lookbehind /.(?<=r)/ 
+# src="url" ===> /(?<=src=")(.*)(?=")/ --> url
+gerisinde src=" olan ve devamin da sadece tirnak olan ifadeyi sec.
+====>>>>> .replace(/\s+/g, ' ').trim() cumledeki extra spaceleri kaldirir
+*/
+
+/*
+--------   6   ------------------
+prime number
+
+Bir tamsayı bağımsız değişkeni alan ve tamsayının asal olup olmamasına bağlı olarak doğru veya yanlış mantıksal bir değer döndüren bir işlev tanımlayın.
+
+  ##Size bir tamsayı girişi verileceğini varsayabilirsiniz.
+  ##Tam sayının yalnızca pozitif olacağını varsayamazsınız. Size negatif sayılar da verilebilir ( veya 0 ).
+ */
+
+const sayz = -1;
+const say = Math.sign(sayz) === -1 ? sayz * -1 : sayz; //(sayinin negatif yada pozitiflik durumunu test eder)
+
+// const say = Math.sign(num) === -1 ? num * -1 : num;
+// function isPrime(say) {
+//   let flag = true;
+//   if (say > 1) {
+//     for (let i = 2; i < say; i++) {
+//       if (say % i === 0) {
+//         flag = false;
+//         break;
+//       }
+//     }
+//     if (flag) {
+//       return true;
+//     } else {
+//       return false;
+//     }
+//   } else {
+//     return false;
+//   }
+// }
+
+function isPrime(say) {
+  let flag = true;
+  const limit = Math.sqrt(num);
+  if (say > 1) {
+    for (let i = 2; i < limit; i++) {
+      if (say % i === 0) {
+        flag = false;
+        break;
+      }
+    }
+    if (flag) {
+      return true;
+    } else {
+      return false;
+    }
+  } else {
+    return false;
+  }
 }
 
-console.log(newName);
+// function isPrime(num) {
+//   if (num < 2) return false;
+//   const limit = Math.sqrt(num);
+//   for (let i = 2; i <= limit; ++i) {
+//     if (num % i === 0) {
+//       return false;
+//     }
+//   }
+//   return true;
+// }
+
+/*-------    7   -----------------------
+Bir veya daha fazla kelimeden oluşan bir dize alan ve aynı dizeyi döndüren, ancak beş veya daha fazla harfli kelimenin tümü ters çevrilmiş bir işlev yazın.Geçirilen dizeler yalnızca harflerden ve boşluklardan oluşacaktır. Boşluklar, yalnızca birden fazla kelime mevcut olduğunda dahil edilecektir.
+
+beispiel ==>
+spinWords( "Hey fellow warriors" ) => returns "Hey wollef sroirraw" 
+spinWords( "This is a test") => returns "This is a test" 
+spinWords( "This is another test" )=> returns "This is rehtona test"
+
+*/
+function spinWords(string) {
+  let spin = string.split(' ');
+
+  for (i of spin) {
+    if (i.split('').length >= 5) {
+      i = i.split('').reverse().join('');
+    }
+  }
+  return spin.join(' ');
+}
+console.log(spinWords('This is another test'));
+
+/* ----------- 8 -----------------
+Size tamsayılar içeren bir dizi (en az 3 uzunluğa sahip olacak, ancak çok büyük olabilir) verilir. Dizi ya tamamen tek tam sayılardan oluşur ya da tek bir N tamsayısı dışında tamamen çift tam sayılardan oluşur. Diziyi argüman olarak alan ve bu "outlier" N değerini döndüren bir yöntem yazın.
+
+beispiel ====>>
+
+[2, 4, 0, 100, 4, 11, 2602, 36]
+Should return: 11 (the only odd number)
+
+[160, 3, 1719, 19, 11, 13, -21]
+Should return: 160 (the only even number)
+
+
+*/
+function findOutlier(integers) {
+  let evenNummer = [];
+  let oddNummer = [];
+
+  for (i of integers) {
+    i % 2 == 0 ? evenNummer.push(i) : oddNummer.push(i);
+  }
+
+  return evenNummer.length > oddNummer.length
+    ? Number(oddNummer.toString())
+    : Number(evenNummer.toString());
+}
+
+console.log(findOutlier([2, 4, 0, 100, 4, 11, 2602, 36]));
+
+function findOutlier(int) {
+  var even = int.filter((a) => a % 2 == 0);
+  var odd = int.filter((a) => a % 2 !== 0);
+  return even.length == 1 ? even[0] : odd[0];
+}
